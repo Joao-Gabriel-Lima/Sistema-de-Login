4580ab5081cdf15ed06886a6d3aca632796dde82
João Gabriel Lima <joao.gls2009@gmail.com>
2026-08-24 23:56:36 -0300

    adicionado sistema de codificação de senhas completo usando hash

--- Server.js
+++ Server.js
@@ -16,17 +16,28 @@ app.post("/cadastro", (req, res) => {
     usuarios.push({ usuario: req.body.usuario, senha: hash });
     fs.writeFileSync("usuarios.json", JSON.stringify(usuarios));
 
-  res.json({ message: "ok" });
-})});
+    res.json({ message: "ok" });
+  });
+});
 
 app.post("/login", (req, res) => {
   const dados = fs.readFileSync("usuarios.json", "utf8");
   const usuarios = JSON.parse(dados);
   const usuarioEncontrado = usuarios.find(function (u) {
-    return u.usuario === req.body.usuario && u.senha === req.body.senha;
+    return u.usuario === req.body.usuario;
   });
   if (usuarioEncontrado) {
-    res.json({ message: "Usuário autenticado com sucesso" });
+    bcrypt.compare(
+      req.body.senha,
+      usuarioEncontrado.senha,
+      function (err, result) {
+        if (result) {
+          res.json({ message: "Usuário autenticado com sucesso" });
+        } else {
+          res.status(401).json({ message: "Usuário ou senha inválidos" });
+        }
+      },
+    );
   } else {
     res.status(401).json({ message: "Usuário ou senha inválidos" });
   }
